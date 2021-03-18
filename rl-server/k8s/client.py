3883341c39e6fd9f8b19db884792e40ec6db8276
charstal <charstal_h@outlook.com>
2021-03-18 09:59:59 +0800

    finish k8s client

--- rl-server/k8s/client.py
+++ rl-server/k8s/client.py
@@ -6,12 +6,12 @@ MEMORY = "MEMORY"
 
 config.load_kube_config()
 
+
 class K8sClient:
     def __init__(self):
         # Configs can be set in Configuration class directly or using helper utility
         self._core_client = client.CoreV1Api()
         self._custom_api_client = client.CustomObjectsApi()
-
 
     def get_all_node_capacity(self):
         """get node capacity
@@ -23,14 +23,21 @@ class K8sClient:
                 "cpu": cpu_convert(i.status.allocatable['cpu']),
                 "memory": memory_convert(i.status.allocatable['memory']),
             }
-            print(i)
         return capacity_map
-    
+
     def get_all_node_usage(self):
         """get node usage
         """
-        print(self._custom_api_client.list_cluster_custom_object('metrics.k8s.io', 'v1beta1', 'nodes'))
-        
+        usage_map = dict()
+        ret = self._custom_api_client.list_cluster_custom_object(
+            'metrics.k8s.io', 'v1beta1', 'nodes')
+        for i in ret['items']:
+            usage_map[i['metadata']['name']] = {
+                "cpu": cpu_convert(i['usage']['cpu']),
+                "memory": memory_convert(i['usage']['memory']),
+            }
+        return usage_map
+
 
 def cpu_convert(src):
     if src.endswith("m"):
@@ -39,6 +46,7 @@ def cpu_convert(src):
         return int(float(src) * 1000)
     except ValueError:
         return 0
+
 
 def memory_convert(src):
     factor = 1000
@@ -50,15 +58,12 @@ def memory_convert(src):
         return int(src)
     except ValueError:
         digit = int(src[:len(src)-1])
-        print(digit)
         return int(digit * math.pow(factor, table[src[len(src)-1]]))
-        
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     k8sclient = K8sClient()
     # print(k8sclient.get_all_node_capacity())
     print(k8sclient.get_all_node_usage())
     # print(cpu_convert("12"))
     # print(memory_convert("123Mi"))
-
