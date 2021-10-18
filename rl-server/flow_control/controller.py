a68439b13aa1c15c22498db4a5759e6c033daf21
charstal <charstal_h@outlook.com>
2021-10-18 15:23:06 +0800

    flow controll

---
+++ rl-server/flow_control/controller.py
@@ -0,0 +1,26 @@
+import datetime
+
+
+class FlowController:
+    # lecky bucket
+    def __init__(self, capacity=20, rate=3):
+        self.capacity = capacity
+        self.last_time = datetime.datetime.now()
+        self.water = 10
+        self.rate = rate
+
+    def grant(self):
+        now = datetime.datetime.now()
+
+        water = max(0, self.water - (now-self.last_time).seconds * self.rate)
+
+        self.last_time = now
+        if ((water + 1) < self.capacity):
+            self.water += 1
+            return True
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    fc = FlowController(size=30)
